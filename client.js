5c09b25b56b4eff0d3ccbcbc87f88be3d430ae6e
StevenJoeZhang <1119186082@qq.com>
2019-04-19 00:54:13 +0800

    remove control

--- client.js
+++ client.js
@@ -3,7 +3,6 @@ var io = require("socket.io-client");
 var World = require("./src/mode/player");
 var Renderer = require("./src/renderer");
 var MusicPlayer = require("./src/musicplayer");
-var Control = require("./src/control");
 var config = require("./config.json");
 
 // Engine globals
@@ -32,7 +31,6 @@ window.onload = function() {
 			"death": ["fx/death.ogg"],
 			"bounce": ["fx/bounce.ogg"],
 		});
-	window.control = new Control();
 	window.world = new World();
 	// Initialize the World
 	world.load_level();
@@ -56,4 +54,71 @@ window.onload = function() {
 		world.update();
 		requestAnimFrame(animloop, canvas);
 	})();
+
+	register();
+}
+
+function register() {
+	// Event registration
+	window.addEventListener("keydown", function(e) {
+		var code;
+		if (!e) var e = window.event;
+		if (e.keyCode) code = e.keyCode;
+		else if (e.which) code = e.which;
+		switch (code) {
+			case 72: // H
+				world.toggle_help();
+				break;
+			case 77: // M
+				music.mute();
+				break;
+			case 78: // N
+				music.next_song();
+				break;
+			case 32: // Space
+			case 80: // P
+				world.pause();
+				break;
+			case 82: // R
+				world.load_level();
+				break;
+			case 83: // S
+				world.shadows = !world.shadows;
+				break;
+		}
+	}, false);
+	window.addEventListener("blur", function() {
+		world.pause(true);
+	}, false);
+	document.getElementById("mute").addEventListener("click", function() {
+		music.mute();
+	}, false);
+	document.getElementById("newlevel").addEventListener("click", function() {
+		world.load_level();
+	}, false);
+	document.getElementById("pause").addEventListener("click", function() {
+		world.pause();
+	}, false);
+	document.getElementById("help").addEventListener("click", function() {
+		world.toggle_help();
+	}, false);
+	document.getElementById("messages").addEventListener("click", function() {
+		switch (document.getElementById("messages").className) {
+			case "paused":
+				world.pause();
+				break;
+			case "death":
+			case "warning":
+			case "success":
+				world.load_level();
+				break;
+			default:
+				break;
+		}
+	}, false);
+	document.getElementById("playbutton").addEventListener("click", function() {
+		world.toggle_help();
+		// Play a sound in order to allow any sound playback at all on iOS
+		music.play_sound("win");
+	}, false);
 }
